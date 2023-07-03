74f01161806c5a1583a3662593ed9c2302a26ae3
ikandelaki2 <irakli.kandelaki@scandiweb.com>
2023-07-03 17:46:27 +0400

    Adjust import functionality

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -12,9 +12,9 @@ import { createClient } from 'redis';
 import { createAdapter } from '@socket.io/redis-adapter';
 import Logger from 'bunyan';
 import 'express-async-errors';
-import { config } from './config';
-import applicationRoutes from './routes';
-import { CustomError, IErrorResponse } from './shared/globals/helpers/error-handler';
+import { CustomError, IErrorResponse } from '@global/helpers/error-handler';
+import { config } from '@root/config';
+import applicationRoutes from '@root/routes';
 
 const SERVER_PORT = 5000;
 const log: Logger = config.createLogger('server');
@@ -114,5 +114,7 @@ export class ChattyServer {
     });
   }
 
-  private socketIOConnections(io: Server): void {}
+  private socketIOConnections(io: Server): void {
+    log.info('socketIOConnections');
+  }
 }
